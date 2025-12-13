3f33878702ce87da015210c6867ff45ad75f9475
yasir arfat <yasir.arfat.cse@gmail.com>
2025-12-13 09:29:36 +0530

    resume-updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// ✅ Import Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 // (Optional) Keep your custom CSS too
